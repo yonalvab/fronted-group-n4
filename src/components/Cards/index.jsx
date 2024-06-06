6ee7863e4d1c2c5e462b99a5e278416de2a40581
VianGalop <matcaptu10@gmail.com>
2024-06-06 00:23:01 -0600

    cartas con imagen miniatura listo

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import miniatura from '../../assets/61.jpg'
 
-export const Cards = () => {
+export const Cards = ({ miniatura, titulo, descripcion}) => {
+    /* console.log(miniatura, titulo, descripcion); */
     return (
         <>
-            <div
-                className="relative rounded-lg -skew-x-3 -translate-y-2  hover:-translate-y-1 hover:-translate-x-0 hover:skew-x-0 duration-500 w-72 h-44 p-2 bg-neutral-50 card-compact hover:bg-base-200 transition-all [box-shadow:10px_10px_#9299A3] hover:[box-shadow:4px_4px_#9299A3]"
+            <div className="relative rounded-lg -skew-x-3 -translate-y-2  hover:-translate-y-1 hover:-translate-x-0 hover:skew-x-0 duration-500 w-72 h-56 p-2 mb- bg-neutral-50 card-compact hover:bg-base-200 transition-all [box-shadow:10px_10px_#9299A3] hover:[box-shadow:4px_4px_#9299A3]"
             >
-                <figure className="w-full h-full">
+                <div className="w-full h-3/4 mb-8 pb-4">
                     <img
-                        src={miniatura}
-                        alt="change to a img tag"
-                        className=" text-neutral-50 min-h-full rounded-lg border border-opacity-5"
-                    ></img>
-                </figure>
-                <div className="absolute text-neutral-50 bottom-4 left-0 px-4">
-                    <span className="font-bold">Tittle</span>
+                        src={miniatura? miniatura:'images.png'}
+                        alt="No encontrado"
+                        className="min-h-full rounded-lg border border-opacity-5 "
+                    />
+                </div>
+                <div className="absolute text-black bottom-4 left-0 px-4 gap-4">
+                    <span className="font-bold  text-blue-700">{titulo? titulo:'Hola'}</span>
                     <p className="text-sm opacity-80 line-clamp-2">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget
-                        feugiat orci. Curabitur efficitur elit arcu, a luctus sapien luctus ut.
+                        {descripcion? descripcion: 'descripcion'}
                     </p>
                 </div>
             </div>
--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Header } from '../Header'
 import { Cards } from '../Cards'
 import { InfoUser } from '../InfoUser'
+import axios from 'axios'
 
 export const Dashboard = () => {
+
+    const [miniVideos, setMiniVideos] = useState([])
+    useEffect(() => {
+      const fetchVideos = async () => {
+        try {
+          const response = await axios.get('http://localhost:3000/api/videos/usuario/6660a38314a920cdddc220d9')
+          setMiniVideos(response.data.listaDeVideos)
+          console.log(miniVideos);
+        } catch (error) {
+          console.error('Error fetching miniatura:', error)
+        }
+      }
+  
+      fetchVideos()
+    }, [])
+
     return (
         <>
 
             <div className=' flex gap-7 ' >
                 <article className='w-full h-[640px] bg-slate-300 rounded-3xl px-16 py-14 grid grid-cols-2 gap-8 overflow-y-auto'>
-                    <Cards />
-                    <Cards />
-                    <Cards />
-                    <Cards />
-                    <Cards />
-                    <Cards />
-                    <Cards />
-                    <Cards />
+                {miniVideos.map(video => (
+                    <Cards key={video._id} miniatura={`http://localhost:3000/api/videos/${video.miniatura}`} titulo={video.titulo} descripcion={video.descripcion}/> 
+                ))}
+
                 </article>
                 <InfoUser />
             </div>
